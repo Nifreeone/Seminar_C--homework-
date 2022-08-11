3cd3d8c68ded75e70623d663c3c4582529029fa1
niyaz <xamidyll@gmail.com>
2022-08-11 12:41:07 +0300

    Seminar2 homework

---
+++ Seminar2/task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число = ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int number1 = a / 10;
+int number2 = number1 % 10;
+
+    Console.WriteLine(number2);
+
---
+++ Seminar2/task15/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите порядковое число дня");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 2:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 3:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 4:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 5:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели");
+        break;
+
+
+}
