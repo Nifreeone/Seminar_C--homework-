03fa889ef69f3bfdb42950be2a1bcf0febca8419
niyaz <xamidyll@gmail.com>
2022-08-21 22:54:17 +0300

    Seminar5

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[12];
+Random rand = new Random();
+int n = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = rand.Next(100, 1000);
+if (array[i] % 2 == 0)
+{
+    n = n + 1;
+}
+}
+
+Console.WriteLine(string.Join(", ", array));
+Console.Write(n);
---
+++ Seminar5/Task39/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+// от 0 до 1 это по сути тоже вещественные числа, поэтому оставил как есть, округлить числа только в массиве 
+// не получилось не с :f2, не с tofixed(2), буду благодарен, если подскажите как
+double[] array = new double[8];
+Random rand = new Random();
+double max = 0;
+double min = 1;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.NextDouble();
+
+}
+for (int j = 0; j < array.Length; j++)
+{
+    if (max < array[j])
+        max = array[j];
+
+    else if (min > array[j])
+        min = array[j];
+}
+
+double r = max - min;
+Console.WriteLine(string.Join(", ", array));
+Console.Write($" Минимум={min}  Максимум={max}, Разница между ними {r}");
