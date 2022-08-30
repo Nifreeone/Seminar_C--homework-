ad968ce24101117baf7d88e4458b81e7be8fc55c
niyaz <xamidyll@gmail.com>
2022-08-30 19:39:42 +0300

    task54

---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+//каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк и столбцов через Enter и задайте массив ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 1, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int tmp = matrix[0, 0];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int c = 0; c < matrix.GetLength(1); c++)
+        {
+            if (matrix[i, j] > matrix[i, c])
+            {
+                tmp = matrix[i, j];
+                matrix[i, j] = matrix[i, c];
+                matrix[i, c] = tmp;
+            }
+        }
+    }
+}
+Console.WriteLine(" ");
+PrintMatrix(matrix);
+
